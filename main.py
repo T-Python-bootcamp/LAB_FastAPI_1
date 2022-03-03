b65657d353f2c50b28cda5a78ec2e5e6f0388731
Mohammmed1101 <92180726+Mohammmed1101@users.noreply.github.com>
2022-03-03 17:41:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI 
+from typing import Optional
 
 app = FastAPI()
 
@@ -54,43 +55,66 @@ async def get_a_student(student_id: int):
 
 
 
-# students = {
-#     1: {
-#         "Student Id":1 , 
-#         "Student Name" : "Ahmed",
-#         "GPA" : 3.6
-#     },
-#     2: {
-#         "Student Id": 2,
-#         "Student Name": "Peter",
-#         "GPA" : 3.6
-#     } ,  
-#     3: {
-#         "Student Id":3,
-#         "Student Name": "Ahmed",
-#         "GPA" : 3.6
-#     } , 
-#     4: {
-#         "Student Id":4,
-#         "Student Name": "Ahmed",
-#         "GPA" : 3.6
-#     } ,   
-#     5: {
-#         "Student Id":5,
-#         "Student Name": "Ahmed",
-#         "GPA" : 3.6
-#     } , 
-#     6: {
-#         "Student Id":6,
-#         "Student Name": "Ahmed",
-#         "GPA" : 3.6
-#     } , 
-#     7: {
-#         "Student Id":7,
-#         "Student Name": "Ahmed",
-#         "GPA" : 3.6
-#     }
-# }
+students = {
+    1: {
+        "Student Id":1 , 
+        "Student Name" : "Ahmed",
+        "GPA" : 3.6
+    },
+    2: {
+        "Student Id": 2,
+        "Student Name": "Peter",
+        "GPA" : 3.6
+    } ,  
+    3: {
+        "Student Id":3,
+        "Student Name": "Ahmed",
+        "GPA" : 3.6
+    } , 
+    4: {
+        "Student Id":4,
+        "Student Name": "Ahmed",
+        "GPA" : 3.6
+    } ,   
+    5: {
+        "Student Id":5,
+        "Student Name": "Ahmed",
+        "GPA" : 3.6
+    } , 
+    6: {
+        "Student Id":6,
+        "Student Name": "Ahmed",
+        "GPA" : 3.6
+    } , 
+    7: {
+        "Student Id":7,
+        "Student Name": "Ahmed",
+        "GPA" : 3.6
+    }
+}
+items={
+     1: {
+        "items" :1
+        
+    },
+    2: {
+         "items" :2
+    } ,  
+    3: {
+        "items" :3
+    } 
+}
+
+
+@app.get("/students")
+async def getStudent(skip:int=0,limit :int=7,word:Optional[str] = None):
+    return students[skip : skip + limit]
 
 
 
+
+@app.get("/students/{student_id}/items/{item_id}")
+async def read_item_student(student_id :int , item_id:int):
+    item= items[item_id]
+    student=students[student_id]
+    return student,item
